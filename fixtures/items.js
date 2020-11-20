87c2d321b6fe8f42934ad3717a60116c69aa7fb8
juunini <juuni.ni.i@gmail.com>
2020-11-20 20:59:53 +0900

    Add sample data shops and items

---
+++ fixtures/items.js
@@ -0,0 +1,124 @@
+const items = [
+  {
+    brand: 'DISTURBIA',
+    name: 'Stellar Oversized Jumper',
+    price: '48',
+    currency: 'GBP',
+    href: 'https://www.disturbia.co.uk/products/womens-all-tops/Stellar-Oversized-Jumper',
+    images: [
+      'https://www.disturbia.co.uk/products/womens-all-tops/Stellar-Oversized-Jumper/image/15797.jpeg',
+      'https://www.disturbia.co.uk/products/womens-all-tops/Stellar-Oversized-Jumper/image/15796.jpeg',
+      'https://www.disturbia.co.uk/products/womens-all-tops/Stellar-Oversized-Jumper/image/15798.jpeg',
+      'https://www.disturbia.co.uk/products/womens-all-tops/Stellar-Oversized-Jumper/image/15794.jpeg',
+      'https://www.disturbia.co.uk/products/womens-all-tops/Stellar-Oversized-Jumper/image/15795.jpeg',
+      'https://www.disturbia.co.uk/products/womens-all-tops/Stellar-Oversized-Jumper/image/15933.jpeg',
+      'https://www.disturbia.co.uk/products/womens-all-tops/Stellar-Oversized-Jumper/image/15934.jpeg',
+      'https://www.disturbia.co.uk/products/womens-all-tops/Stellar-Oversized-Jumper/image/15793.jpeg',
+    ],
+    sizes: ['XS-S', 'M-L', 'XL-2XL'],
+    description: `Fine knit, super soft crew neck jumper.
+
+Star and moon pattern.
+
+Oversized fit.`,
+  },
+  {
+    brand: 'DISTURBIA',
+    name: 'Toadstool Midi Dress',
+    price: 42,
+    currency: 'GBP',
+    images: [
+      'https://www.disturbia.co.uk/products/womens-dresses/toadstool-midi-dress/image/15222.jpeg',
+      'https://www.disturbia.co.uk/products/womens-dresses/toadstool-midi-dress/image/15317.jpeg',
+      'https://www.disturbia.co.uk/products/womens-dresses/toadstool-midi-dress/image/15223.jpeg',
+      'https://www.disturbia.co.uk/products/womens-dresses/toadstool-midi-dress/image/15226.jpeg',
+      'https://www.disturbia.co.uk/products/womens-dresses/toadstool-midi-dress/image/15225.jpeg',
+    ],
+    href: 'https://www.disturbia.co.uk/products/womens-dresses/toadstool-midi-dress',
+    sizes: [],
+    description: `Button up midi dress with skirt vents.
+
+All over Fly Agaric mushroom autumnal digital print. 
+
+Non stretch fabric.
+
+100% Polyester.`,
+  },
+  {
+    brand: 'KILLSTAR',
+    name: 'Empire Studded Boots',
+    price: 94.99,
+    currency: 'GBP',
+    images: [
+      'https://cdn.shopify.com/s/files/1/0228/2373/products/EMPIRE-BOOTS-B_x1600.jpg?v=1605183751',
+      'https://cdn.shopify.com/s/files/1/0228/2373/products/EMPIRE-BOOTS-C_x1600.jpg?v=1605183751',
+      'https://cdn.shopify.com/s/files/1/0228/2373/products/EMPIRE-BOOTS_x1600.jpg?v=1605183751',
+    ],
+    sizes: ['UK3', 'UK4', 'UK5', 'UK6', 'UK7', 'UK8', 'UK9'],
+    description: `EMPIRE.
+
+You'll turn up ordinary if you are not careful.
+
+- Vegan Leather [Forever].
+- Studs All Over.
+- Lace-Up Front.
+- Side Zip.
+- Platform - 6cm/2.3”.
+- Weight 2195 grams.
+
+Make 'em look twice with envy with the oh-so-fierce 'Empire' boots. In a luxe faux leather with lace up front - featuring studs all over and zips at the sides. Great for adding some serious attitude for yer day-to-day adventures out the crypt or causing havoc at night!
+
+Match as yer immortal heart desires - looks killer with everything!
+
+With KILLSTAR Branding. 100% PU.`,
+  },
+  {
+    brand: 'KILLSTAR',
+    name: 'Pagan Round Blanket [B]',
+    price: 29.99,
+    currency: 'GBP',
+    images: [
+      'https://cdn.shopify.com/s/files/1/0228/2373/products/PAGAN-BLANKET-C_x1600.jpg?v=1561636961',
+      'https://cdn.shopify.com/s/files/1/0228/2373/products/PAGAN-BLANKET_72cdf805-c1e3-464d-9438-bb8a881fa5e6_x1600.jpg?v=1591792830',
+      'https://cdn.shopify.com/s/files/1/0228/2373/products/PAGAN-BLANKET-B_381ad382-8fd0-4df8-b4c1-f89cf4763663_x1600.jpg?v=1591792830',
+    ],
+    sizes: ['One Size'],
+    description: `PAGAN.
+
+Sure, u can pray for me - and I'll dance naked in the forest for you.
+
+- Soft Touch Fabric.
+- Round Blanket, with Fringes.
+- Huge Graphic Print
+- Size 158cm x 158cm
+
+Looking to complete yer crypt with some killer details? Need a cool af meditation blanket - or just a partner in yer rituals? The 'Pagan' round blanket is in a lush soft fabric - with a huge contrast graphic of the pagan calendar, accent starts and moon phases. Huge - so you know you get the desired effect!
+
+Completes yer life perfectly, but don't snooze - we only got limited qty!
+
+with KILLSTAR branding, 100% Polyester.`,
+  },
+  {
+    brand: 'ROGUE AND WOLF',
+    name: 'Lotus Awakens Armour Ring',
+    price: 36,
+    currency: 'GBP',
+    images: [
+      'https://cdn.shopify.com/s/files/1/0872/3036/products/BM_2945_Closeup_ring_1080x.jpg?v=1526393227',
+      'https://cdn.shopify.com/s/files/1/0872/3036/products/230004_1080x.jpg?v=1526393227',
+      'https://cdn.shopify.com/s/files/1/0872/3036/products/230006_d434750a-1405-4fbc-8ede-e803cde3c37f_1080x.jpg?v=1526393227',
+      'https://cdn.shopify.com/s/files/1/0872/3036/products/BM_3397_02f64851-c591-493b-aa9d-876f14e811a1_1080x.jpg?v=1526393227',
+    ],
+    sizes: ['US6', 'US7', 'US8', 'US9'],
+    description: `A full finger armour ring, to make your enemies die in envy!
+Fully functional to bend with your finger.
+
+A gentle budding lotus symbol adorns this ring.
+Right here, in this moment, wake up and rise like a lotus.
+
+
+3D printed in polyamide plastic.`,
+  },
+];
+
+export default items;
---
+++ fixtures/shops.js
@@ -0,0 +1,17 @@
+const shops = [
+  {
+    id: 1,
+    name: 'KILLSTAR',
+    sizeGuide: '/killstarSizeGuide.html',
+  },
+  {
+    id: 2,
+    name: 'DISTURBIA',
+  },
+  {
+    id: 3,
+    name: 'Rogue + Wolf',
+  },
+];
+
+export default shops;
