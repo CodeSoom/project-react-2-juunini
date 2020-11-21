afd8d30999d0cdfadacc0f1ed2f80f93a853e62e
juunini <juuni.ni.i@gmail.com>
2020-11-22 01:28:06 +0900

    Add itemReducer in rootReducer

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
 import shopsReducer from './shops';
+import itemsReducer from './items';
 
 const rootReducer = combineReducers({
   shops: shopsReducer,
+  items: itemsReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
