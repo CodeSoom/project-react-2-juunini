d9aabba8f5e89e0867eda819d7aaf128368f2132
juunini <juuni.ni.i@gmail.com>
2020-11-22 17:12:43 +0900

    Path / renders items page

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,40 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import { render, screen } from '@testing-library/react';
 
+import { RootState } from 'src/redux/rootReducer';
+import shops from 'fixtures/shops';
+import items from 'fixtures/items';
 import App from './App';
 
-test('App', () => {
-  render(<App />);
+jest.mock('react-redux');
 
-  expect(screen.getByText(/aaaaa/)).toBeInTheDocument();
+describe('App', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    dispatch.mockClear();
+
+    (useDispatch as jest.Mock).mockImplementation(() => dispatch);
+    (useSelector as jest.Mock)
+      .mockImplementation((selector: (arg: RootState) => void) => selector({
+        shops,
+        items,
+      }));
+  });
+
+  function renderApp({ path }: {path: string}) {
+    return render((
+      <MemoryRouter initialEntries={[path]}>
+        <App />
+      </MemoryRouter>
+    ));
+  }
+
+  context('with path /', () => {
+    it('renders items page', () => {
+      renderApp({ path: '/' });
+    });
+  });
 });
