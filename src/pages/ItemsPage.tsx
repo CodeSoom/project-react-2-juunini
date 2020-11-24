bd99b9b1f3da7d99bdaa823cf642efd239782867
juunini <juuni.ni.i@gmail.com>
2020-11-24 23:34:24 +0900

    Add loadShops in ItemsPage

--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -3,12 +3,14 @@ import { useDispatch } from 'react-redux';
 
 import ItemsContainer from 'src/containers/ItemsContainer';
 import { loadItems } from 'src/redux/items';
+import { loadShops } from 'src/redux/shops';
 
 export default function ItemsPage() {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(loadItems());
+    dispatch(loadShops());
   });
 
   return (<ItemsContainer />);
