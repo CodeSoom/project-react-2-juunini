317e6c6d1e7c63c04059a744addeea0f3e72acb4
juunini <juuni.ni.i@gmail.com>
2020-11-28 20:26:18 +0900

    Add not found page

---
+++ src/pages/__test__/NotFoundPage.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import NotFoundPage from '../NotFoundPage';
+
+test('NotFoundPage', () => {
+  render(<NotFoundPage />);
+
+  expect(screen.getByText('not found')).toBeInTheDocument();
+});
