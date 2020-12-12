0c1c4fbedb7fbeb1012aa07604ef3e0b8ac50f9b
juunini <juuni.ni.i@gmail.com>
2020-12-12 12:37:26 +0900

    Add footer layout

---
+++ src/containers/FooterContainer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Footer } from 'src/layout/Footer';
+
+function FooterContainer() {
+  return (
+    <Footer>
+      Footer
+    </Footer>
+  );
+}
+
+export default React.memo(FooterContainer);
